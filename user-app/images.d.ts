6d84e2e44486a4580e6aa0598121ebdb399bf851
dejongyeong <dejong.re2@gmail.com>
2022-01-28 01:44:24 +0000

    initial commit with ts, eslint-airbnb, prettier, and create-react-app

---
+++ user-app/images.d.ts
@@ -0,0 +1,6 @@
+declare module '*.svg'
+declare module '*.png'
+declare module '*.jpg'
+
+// eslint-disable-next-line max-len
+// Reference: https://stackoverflow.com/questions/51256923/typescript-reactjs-imported-svg-does-not-render

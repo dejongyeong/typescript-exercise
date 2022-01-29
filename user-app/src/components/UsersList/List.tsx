e19c63c0e37fb8d47e6a581b5a57970f70b3bd5a
dejongyeong <dejong.re2@gmail.com>
2022-01-29 20:34:18 +0000

    display list of users on  main listing page

--- user-app/src/components/UsersList/List.tsx
+++ user-app/src/components/UsersList/List.tsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { Table } from "antd";
+import { Link } from "react-router-dom";
+import { EyeFilled } from "@ant-design/icons";
 import IUser from "../../types/Users";
 import Header from "../Header";
 import "./List.css";
@@ -8,10 +11,45 @@ interface IListProps {
 }
 
 export default function List({ users }: IListProps): JSX.Element {
+  const columns = [
+    {
+      title: "ID",
+      dataIndex: "id",
+      key: "id",
+      render: (text: string) => <b>{text}</b>,
+    },
+    {
+      title: "First Name",
+      dataIndex: "first_name",
+      key: "first_name",
+    },
+    {
+      title: "Last Name",
+      dataIndex: "last_name",
+      key: "last_name",
+    },
+    {
+      title: "Action",
+      dataIndex: "action",
+      key: "action",
+      render: (_text: string, row: any) => (
+        <Link to={{ pathname: `/users/${row.id}` }}>
+          <EyeFilled />
+        </Link>
+      ),
+    },
+  ];
+
   return (
     <div className='ListContainer'>
-      <Header title='User List' />
-      I&apos;m a List.. starting<p>{JSON.stringify(users)}</p>
+      <Header title='Users' />
+      <Table
+        dataSource={users}
+        columns={columns}
+        size='small'
+        rowKey={(row) => row.id}
+        pagination={{ position: ["bottomRight"], showSizeChanger: false }}
+      />
     </div>
   );
 }

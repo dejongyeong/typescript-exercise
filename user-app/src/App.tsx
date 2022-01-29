da07faa9beab9ded81db7142f020c1c2bade1441
dejongyeong <dejong.re2@gmail.com>
2022-01-29 22:40:38 +0000

    redirect all other urls to list screen instead of page 404

--- user-app/src/App.tsx
+++ user-app/src/App.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
-import NotFound from "./components/NotFound";
 import UsersList from "./components/UsersList";
 import UserDetails from "./components/UserDetails";
 import "./App.css";
@@ -11,8 +10,7 @@ function App(): JSX.Element {
       <Routes>
         <Route path='/users' element={<UsersList />} />
         <Route path='/users/:id' element={<UserDetails />} />
-        <Route path='/' element={<Navigate to='/users' />} />
-        <Route path='*' element={<NotFound />} />
+        <Route path='*' element={<Navigate to='/users' />} />
       </Routes>
     </div>
   );
